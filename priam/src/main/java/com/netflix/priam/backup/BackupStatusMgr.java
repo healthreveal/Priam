160fa6cfb705b0046254b95cf8dd4b8b028819cc
Frank Norman <fcnorman@gmail.com>
2016-12-15 21:46:02 +0000

    Changed URL in BackupStatusMgr

--- priam/src/main/java/com/netflix/priam/backup/BackupStatusMgr.java
+++ priam/src/main/java/com/netflix/priam/backup/BackupStatusMgr.java
@@ -67,7 +67,7 @@ public class BackupStatusMgr {
 			c.setCompletedTime(completedTime);
 			c.setStartTime(startTime);
 			
-			String token = SystemUtils.getDataFromUrl("http://localhost:8080/Priam/REST/v1/cassconfig/get_token");
+			String token = SystemUtils.getDataFromUrl("http://localhost:8080/priam-web/REST/v1/cassconfig/get_token");
 			c.setToken(token);
 			
 			bkups.add(c);
